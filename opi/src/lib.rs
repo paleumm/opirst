09876af74bcdd0e6afc17e646381febe0e2bdee0
Permpoon B <zdotqdot@gmail.com>
2022-12-26 16:43:57 +0700

    Update lib.rs

--- opi/src/lib.rs
+++ opi/src/lib.rs
@@ -1,3 +1,9 @@
+use std::fs;
+use std::fs::{File, OpenOptions};
+use std::io;
+use std::io::prelude::*;
+use std::os::unix;
+use std::path::Path;
 pub struct OPI5{
     led: LED,
 }
@@ -38,23 +44,43 @@ pub enum Triggering {
 }
 
 impl LED {
+
     pub fn new() -> Self {
         LED { status: Status::On, trigger: Triggering::HeartBeat }
     }
 
     pub fn turn_off(&mut self) {
         self.status = Status::Off;
+        echo("0", &Path::new("/sys/class/leds/status_led/brightness")).unwrap_or_else(|why| {
+            println!("! {:?}", why.kind());
+        });
     }
 
     pub fn turn_on(&mut self) {
         self.status = Status::On;
+        echo("1", &Path::new("/sys/class/leds/status_led/brightness")).unwrap_or_else(|why| {
+            println!("! {:?}", why.kind());
+        });
     }
 
     pub fn blink(&mut self) {
         self.trigger = Triggering::HeartBeat;
+        echo("heartbeat", &Path::new("/sys/class/leds/status_led/trigger")).unwrap_or_else(|why| {
+            println!("! {:?}", why.kind());
+        });
     }
 
     pub fn stop_blink(&mut self) {
         self.trigger = Triggering::None;
+        echo("none", &Path::new("/sys/class/leds/status_led/trigger")).unwrap_or_else(|why| {
+            println!("! {:?}", why.kind());
+        });
     }
+}
+
+// A simple implementation of `% echo s > path`
+pub fn echo(s: &str, path: &Path) -> io::Result<()> {
+    let mut f = File::create(path)?;
+
+    f.write_all(s.as_bytes())
 }
