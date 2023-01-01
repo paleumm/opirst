d6661345139b1834a39b51aa338f1183aeee292f
Permpoon B <zdotqdot@gmail.com>
2023-01-01 14:26:03 +0700

    Update temp.rs

--- opi/src/utils/temp.rs
+++ opi/src/utils/temp.rs
@@ -1,4 +1,4 @@
-// use std::process::Command;
+use std::process::Command;
 
 pub struct TEMP {
     temp: f32,
@@ -6,6 +6,8 @@ pub struct TEMP {
 
 impl TEMP {
     pub fn new() -> Self {
+        let out = Command::new("sensors").output().unwrap();
+        println!("printing:\n{}", String::from_utf8(out.stdout).unwrap());
         TEMP { temp: 0.0 }
     }
 
